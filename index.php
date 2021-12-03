fd1fc84775045e9bfff1dcafb0a0afbd9c99e8f0
DmitrySolomatin <mrnyancthulhu@gmail.com>
2021-12-03 09:37:39 +0000

    Congrats!

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>Congratulations!</h1>
-        <p><em>LEEEROYYY JEEENKINS!</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <p><em>LEEEROOOY JEEENKINS!</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
     </section>
